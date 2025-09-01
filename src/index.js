d024fbef31209a27d655960bde1ed72611b3d107
zooblii <zooblie0@gmail.com>
2025-09-01 14:33:10 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(number) {
+  const s = Math.abs(number).toString();
+  let result = '';
+
+  for (let i = s.length - 1; i >= 0; i -= 1) {
+    result += s[i];
+  }
+
+  return Number(result);
 };
